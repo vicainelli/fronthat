021c74f5205ff5d48e87e221ee4022ad950be974
Karl Ranna <karl@karlranna.com>
2017-04-30 22:34:56 +0300

    Fix return 404 status code

--- app/not-found/route.js
+++ app/not-found/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  fastboot: Ember.inject.service(),
+
+  model() {
+    if (this.get('fastboot.isFastBoot')) {
+      this.set('fastboot.response.statusCode', 404);
+    }
+  }
 });
