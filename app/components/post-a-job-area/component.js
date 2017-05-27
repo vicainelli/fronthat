82dd3a37f53da7687f6abd63ff525edcfb866ac5
krrprr <krista.partel@gmail.com>
2017-05-27 12:23:43 +0300

    initial design for post a job form

--- app/components/post-a-job-area/component.js
+++ app/components/post-a-job-area/component.js
@@ -4,86 +4,90 @@ import hbs from 'htmlbars-inline-precompile';
 export default Ember.Component.extend({
 
   layout: hbs`
-    <div data-test-post-a-job-area-container>
+    <div data-test-post-a-job-area-container class="post-a-job-area">
 
       <div class="input container" data-test-title-input-container>
         <label for="title-input" data-test-title-input-label>Title</label>
-        {{one-way-input
-          title.value
-          id="title-input"
-          update=updateTitle
-        }}
-        {{#if title.errors}}
-          <span data-test-title-input-errors>
-            {{title.errors}}
-          </span>
-        {{/if}}
+        <div class="input-and-error">
+          {{one-way-input
+            title.value
+            id="title-input"
+            update=updateTitle
+          }}
+          {{#if title.errors}}
+            <span data-test-title-input-errors>
+              {{title.errors}}
+            </span>
+          {{/if}}
+        </div>
       </div>
 
       <div class="input container" data-test-url-input-container>
         <label for="url-input" data-test-url-input-label>URL</label>
-        {{one-way-input
-          url.value
-          id="url-input"
-          update=updateURL
-        }}
-        {{#if url.errors}}
-          <span data-test-url-input-errors>
-            {{url.errors}}
-          </span>
-        {{/if}}
-        <span class="url-input-tip">
-          URL to apply: https://yourcompany.com/careers/apply
-        </span>
+        <div class="input-and-error">
+          {{one-way-input
+            url.value
+            id="url-input"
+            update=updateURL
+            placeholder="URL to apply: https://yourcompany.com/careers/apply"
+          }}
+          {{#if url.errors}}
+            <span data-test-url-input-errors>
+              {{url.errors}}
+            </span>
+          {{/if}}
+        </div>
       </div>
 
       <div class="input container" data-test-description-input-container>
         <label for="description-input" data-test-description-input-label>Description</label>
-        {{one-way-textarea
-          description.value
-          id="description-input"
-          update=updateDescription
-        }}
-        {{#if description.errors}}
-          <span data-test-description-input-errors>
-            {{description.errors}}
-          </span>
-        {{/if}}
+        <div class="input-and-error">
+          {{one-way-textarea
+            description.value
+            id="description-input"
+            update=updateDescription
+          }}
+          {{#if description.errors}}
+            <span data-test-description-input-errors>
+              {{description.errors}}
+            </span>
+          {{/if}}
+        </div>
       </div>
 
       <div class="input container" data-test-name-input-container>
         <label for="name-input" data-test-name-input-label>Name</label>
-        {{one-way-input
-          name.value
-          id="name-input"
-          update=updateName
-        }}
-        {{#if name.errors}}
-          <span data-test-name-input-errors>
-            {{name.errors}}
-          </span>
-        {{/if}}
-        <span class="name-input-tip">
-          Enter your or company name.
-        </span>
+        <div class="input-and-error">
+          {{one-way-input
+            name.value
+            id="name-input"
+            update=updateName
+            placeholder="Enter your or company name"
+          }}
+          {{#if name.errors}}
+            <span data-test-name-input-errors>
+              {{name.errors}}
+            </span>
+          {{/if}}
+        </div>
       </div>
 
       <div class="input container" data-test-email-input-container>
         <label for="email-input" data-test-email-input-label>Email</label>
-        {{one-way-input
-          email.value
-          id="email-input"
-          update=updateEmail
-          type='email'
-        }}
-        {{#if email.errors}}
-          <span data-test-email-input-errors>
-            {{email.errors}}
-          </span>
-        {{/if}}
-        <span class="email-input-tip">
-          For receipt and confirmation email.
-        </span>
+        <div class="input-and-error">
+          {{one-way-input
+            email.value
+            id="email-input"
+            update=updateEmail
+            type='email'
+            placeholder="Enter email for receipt and confirmation email"
+          }}
+          {{#if email.errors}}
+            <span data-test-email-input-errors>
+              {{email.errors}}
+            </span>
+          {{/if}}
+        </div>
       </div>
 
       {{#if errors}}
