4c992959301ba0695439d48976700e7e5595bd43
krrprr <krista.partel@gmail.com>
2017-05-28 12:51:07 +0300

    Change post a job form design

--- app/components/post-a-job-area/component.js
+++ app/components/post-a-job-area/component.js
@@ -6,75 +6,82 @@ export default Ember.Component.extend({
   layout: hbs`
     <div data-test-post-a-job-area-container class="post-a-job-area">
 
-      <div class="input container" data-test-title-input-container>
-        <label for="title-input" data-test-title-input-label>Title</label>
-        <div class="input-and-error">
+      <form>
+        <div class="input container" data-test-title-input-container>
+          <div class="label-and-error">
+            <label for="title-input" data-test-title-input-label>Title</label>
+            {{#if title.errors}}
+              <span data-test-title-input-errors>
+                {{title.errors}}
+              </span>
+            {{/if}}
+          </div>
           {{one-way-input
             title.value
             id="title-input"
             update=updateTitle
           }}
-          {{#if title.errors}}
-            <span data-test-title-input-errors>
-              {{title.errors}}
-            </span>
-          {{/if}}
         </div>
-      </div>
 
-      <div class="input container" data-test-url-input-container>
-        <label for="url-input" data-test-url-input-label>URL</label>
-        <div class="input-and-error">
+        <div class="input container" data-test-url-input-container>
+          <div class="label-and-error">
+            <label for="url-input" data-test-url-input-label>URL</label>
+            {{#if url.errors}}
+              <span data-test-url-input-errors>
+                {{url.errors}}
+              </span>
+            {{/if}}
+          </div>
           {{one-way-input
             url.value
             id="url-input"
             update=updateURL
             placeholder="URL to apply: https://yourcompany.com/careers/apply"
           }}
-          {{#if url.errors}}
-            <span data-test-url-input-errors>
-              {{url.errors}}
-            </span>
-          {{/if}}
         </div>
-      </div>
 
-      <div class="input container" data-test-description-input-container>
-        <label for="description-input" data-test-description-input-label>Description</label>
-        <div class="input-and-error">
+        <div class="input container" data-test-description-input-container>
+          <div class="label-and-error">
+            <label for="description-input" data-test-description-input-label>Description</label>
+            {{#if description.errors}}
+              <span data-test-description-input-errors>
+                {{description.errors}}
+              </span>
+            {{/if}}
+          </div>
           {{one-way-textarea
             description.value
             id="description-input"
             update=updateDescription
           }}
-          {{#if description.errors}}
-            <span data-test-description-input-errors>
-              {{description.errors}}
-            </span>
-          {{/if}}
         </div>
-      </div>
 
-      <div class="input container" data-test-name-input-container>
-        <label for="name-input" data-test-name-input-label>Name</label>
-        <div class="input-and-error">
+        <div class="input container" data-test-name-input-container>
+          <div class="label-and-error">
+          <label for="name-input" data-test-name-input-label>Name</label>
+            {{#if name.errors}}
+              <span data-test-name-input-errors>
+                {{name.errors}}
+              </span>
+            {{/if}}
+          </div>
           {{one-way-input
             name.value
             id="name-input"
             update=updateName
             placeholder="Enter your or company name"
           }}
-          {{#if name.errors}}
-            <span data-test-name-input-errors>
-              {{name.errors}}
-            </span>
-          {{/if}}
         </div>
-      </div>
 
-      <div class="input container" data-test-email-input-container>
-        <label for="email-input" data-test-email-input-label>Email</label>
-        <div class="input-and-error">
+        <div class="input container" data-test-email-input-container>
+          <div class="label-and-error">
+          <label for="email-input" data-test-email-input-label>Email</label>
+            {{#if email.errors}}
+              <span data-test-email-input-errors>
+                {{email.errors}}
+              </span>
+            {{/if}}
+          </div>
           {{one-way-input
             email.value
             id="email-input"
@@ -82,33 +89,28 @@ export default Ember.Component.extend({
             type='email'
             placeholder="Enter email for receipt and confirmation email"
           }}
-          {{#if email.errors}}
-            <span data-test-email-input-errors>
-              {{email.errors}}
-            </span>
+        </div>
+
+        {{#if errors}}
+          <div class="general-errors-area" data-test-general-errors-area>
+            {{#each errors as |error|}}
+              <div class="general-error">
+                {{error}}
+              </div>
+            {{/each}}
+          </div>
+        {{/if}}
+
+        <div class="submit-area">
+          {{#if disabled}}
+            <input class="nav-button nav-button-dark post-a-job-button disabled" type="submit" name="submit" value="Post a Job" data-test-submit-button-disabled disabled />
+          {{else if loading}}
+            <input class="nav-button nav-button-dark post-a-job-button loading" type="submit" name="submit" value="Posting..." data-test-submit-button-loading />
+          {{else}}
+            <input class="nav-button nav-button-dark post-a-job-button" type="submit" name="submit" value="Post a Job" data-test-submit-button />
           {{/if}}
         </div>
-      </div>
-
-      {{#if errors}}
-        <div class="general-errors-area" data-test-general-errors-area>
-          {{#each errors as |error|}}
-            <div class="general-error">
-              {{error}}
-            </div>
-          {{/each}}
-        </div>
-      {{/if}}
-
-      <div class="submit-area">
-        {{#if disabled}}
-          <input class="nav-button nav-button-dark post-a-job-button disabled" type="submit" name="submit" value="Post a Job" data-test-submit-button-disabled disabled />
-        {{else if loading}}
-          <input class="nav-button nav-button-dark post-a-job-button loading" type="submit" name="submit" value="Posting..." data-test-submit-button-loading />
-        {{else}}
-          <input class="nav-button nav-button-dark post-a-job-button" type="submit" name="submit" value="Post a Job" data-test-submit-button />
-        {{/if}}
-      </div>
+      </form>
 
     </div>
   `
