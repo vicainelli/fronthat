597c514575425c4027af81237a1eb1c2bc5b04a0
Karl Ranna <karl@karlranna.com>
2017-05-29 09:59:17 +0300

    DRY up acceptance tests

--- tests/acceptance/post-a-job-test.js
+++ tests/acceptance/post-a-job-test.js
@@ -4,21 +4,15 @@ import testSelector from 'ember-test-selectors';
 
 moduleForAcceptance('Acceptance | post a job');
 
-test('filling in post a job form', async function(assert) {
-  localStorage.removeItem('reduxPersist:jobs')
-  await visit('/post-a-job');
-  await fillIn('#name-input', 'Russ Hanneman');
-  await fillIn('#email-input', 'russ@siliconvalleyhbo.com');
-  await fillIn('#title-input', 'This is some Title and More');
-  await fillIn('#url-input', 'https://google.com');
-  const generateDescription = () => {
-    let description = '';
-    for (let i=0; i < 255; ++i) {
-      description += 'A';
-    }
-    return description;
-  };
-  await fillIn('#description-input', generateDescription());
+const generateDescription = () => {
+  let description = '';
+  for (let i=0; i < 255; ++i) {
+    description += 'A';
+  }
+  return description;
+};
+
+const assertFormValid = async (assert) => {
   const nameInputErrors = await find(testSelector('name-input-errors'));
   assert.equal(nameInputErrors.length, 0, 'It does not have name input errors');
   const emailInputErrors = await find(testSelector('email-input-errors'));
@@ -29,54 +23,31 @@ test('filling in post a job form', async function(assert) {
   assert.equal(urlInputErrors.length, 0, 'It does not have url input errors');
   const descriptionInputErrors = await find(testSelector('description-input-errors'));
   assert.equal(descriptionInputErrors.length, 0, 'It does not have description input errors');
+};
+
+const assertSubmitButtonEnabled = async (assert) => {
   const submitButton = await find(testSelector('submit-button'));
   assert.equal(submitButton.length, 1, 'Post a Job button is enabled');
-  const API_DELAY = 400;
-  server.timing = API_DELAY;
-  await click(submitButton);
-  const loadingSubmitButton = await find(testSelector('submit-button-loading'));
-  assert.equal(loadingSubmitButton.length, 1, 'Post a Job button is loading');
-  await waitFor(API_DELAY);
-  assert.equal(currentURL(), '/post-a-job/success', 'It redirect to success route');
-});
+  return submitButton;
+};
 
-test('filling in post a job form fails', async function(assert) {
+const assertSubmitButtonDisabled = async (assert) => {
+  const disabledSubmitButton = await find(testSelector('submit-button-disabled'));
+  assert.equal(disabledSubmitButton.length, 1, 'Post a Job button is disabled');
+  return disabledSubmitButton;
+};
+
+const fillInValidForm = async () => {
   localStorage.removeItem('reduxPersist:jobs')
   await visit('/post-a-job');
   await fillIn('#name-input', 'Russ Hanneman');
   await fillIn('#email-input', 'russ@siliconvalleyhbo.com');
   await fillIn('#title-input', 'This is some Title and More');
   await fillIn('#url-input', 'https://google.com');
-  const generateDescription = () => {
-    let description = '';
-    for (let i=0; i < 255; ++i) {
-      description += 'A';
-    }
-    return description;
-  };
   await fillIn('#description-input', generateDescription());
-  const nameInputErrors = await find(testSelector('name-input-errors'));
-  assert.equal(nameInputErrors.length, 0, 'It does not have name input errors');
-  const emailInputErrors = await find(testSelector('email-input-errors'));
-  assert.equal(emailInputErrors.length, 0, 'It does not have email input errors');
-  const titleInputErrors = await find(testSelector('title-input-errors'));
-  assert.equal(titleInputErrors.length, 0, 'It does not have title input errors');
-  const urlInputErrors = await find(testSelector('url-input-errors'));
-  assert.equal(urlInputErrors.length, 0, 'It does not have url input errors');
-  const descriptionInputErrors = await find(testSelector('description-input-errors'));
-  assert.equal(descriptionInputErrors.length, 0, 'It does not have description input errors');
-  const submitButton = await find(testSelector('submit-button'));
-  assert.equal(submitButton.length, 1, 'Post a Job button is enabled');
-  server.post('/jobs', () => {
-    return {};
-  }, 500);
-  await click(submitButton);
-  assert.equal(currentURL(), '/post-a-job', 'It stays on the same route upon failure');
-  const generalErrors = await find(testSelector('general-error'));
-  assert.equal(generalErrors.length, 1, 'It has a general server error');
-});
+};
 
-test('filling in post a job form with errors', async function(assert) {
+const fillInInvalidForm = async () => {
   localStorage.removeItem('reduxPersist:jobs')
   await visit('/post-a-job');
   await fillIn('#name-input', 'AS');
@@ -84,6 +55,9 @@ test('filling in post a job form with errors', async function(assert) {
   await fillIn('#title-input', 'AAA');
   await fillIn('#url-input', 'invalidurl');
   await fillIn('#description-input', 'Too short description');
+};
+
+const assertFormInvalid = async (assert) => {
   const nameInputErrors = await find(testSelector('name-input-errors'));
   assert.equal(nameInputErrors.length, 1, 'It does have name input errors');
   const emailInputErrors = await find(testSelector('email-input-errors'));
@@ -94,23 +68,43 @@ test('filling in post a job form with errors', async function(assert) {
   assert.equal(urlInputErrors.length, 1, 'It does have url input errors');
   const descriptionInputErrors = await find(testSelector('description-input-errors'));
   assert.equal(descriptionInputErrors.length, 1, 'It does have description input errors');
-  const submitButton = await find(testSelector('submit-button-disabled'));
-  assert.equal(submitButton.length, 1, 'Post a Job button is disabled');
+};
+
+test('filling in post a job form', async function(assert) {
+  await fillInValidForm();
+  await assertFormValid(assert);
+  const submitButton = await assertSubmitButtonEnabled(assert);
+  const API_DELAY = 400;
+  server.timing = API_DELAY;
+  await click(submitButton);
+  const loadingSubmitButton = await find(testSelector('submit-button-loading'));
+  assert.equal(loadingSubmitButton.length, 1, 'Post a Job button is loading');
+  await waitFor(API_DELAY);
+  assert.equal(currentURL(), '/post-a-job/success', 'It redirect to success route');
+});
+
+test('filling in post a job form fails', async function(assert) {
+  await fillInValidForm();
+  await assertFormValid(assert);
+  const submitButton = await assertSubmitButtonEnabled(assert);
+  server.post('/jobs', () => {
+    return {};
+  }, 500);
+  await click(submitButton);
+  assert.equal(currentURL(), '/post-a-job', 'It stays on the same route upon failure');
+  const generalErrors = await find(testSelector('general-error'));
+  assert.equal(generalErrors.length, 1, 'It has a general server error');
+});
+
+test('filling in post a job form with errors', async function(assert) {
+  await fillInInvalidForm();
+  await assertFormInvalid(assert);
+  await assertSubmitButtonDisabled(assert);
 });
 
 test('does not display errors on initial load', async function(assert) {
   localStorage.removeItem('reduxPersist:jobs')
   await visit('/post-a-job');
-  const nameInputErrors = await find(testSelector('name-input-errors'));
-  assert.equal(nameInputErrors.length, 0, 'It does not have name input errors');
-  const emailInputErrors = await find(testSelector('email-input-errors'));
-  assert.equal(emailInputErrors.length, 0, 'It does not have email input errors');
-  const titleInputErrors = await find(testSelector('title-input-errors'));
-  assert.equal(titleInputErrors.length, 0, 'It does not have title input errors');
-  const urlInputErrors = await find(testSelector('url-input-errors'));
-  assert.equal(urlInputErrors.length, 0, 'It does not have url input errors');
-  const descriptionInputErrors = await find(testSelector('description-input-errors'));
-  assert.equal(descriptionInputErrors.length, 0, 'It does not have description input errors');
-  const submitButton = await find(testSelector('submit-button-disabled'));
-  assert.equal(submitButton.length, 1, 'Post a Job button is disabled');
+  await assertFormValid(assert);
+  await assertSubmitButtonDisabled(assert);
 });
