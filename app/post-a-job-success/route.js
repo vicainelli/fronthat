b6234729932b83834356fd5696613109f1b00ce2
Karl Ranna <karl@karlranna.com>
2017-06-14 09:06:46 +0300

    Add noindex to post-a-job-success route

--- app/post-a-job-success/route.js
+++ app/post-a-job-success/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel: function(model) {
+    this.setHeadTags(model);
+  },
+  setHeadTags: function() {
+    var headTags = [
+      {
+        type: 'meta',
+        tagId: 'meta-noindex',
+        attrs: {
+          name: 'robots',
+          content: 'noindex',
+        }
+      },
+    ];
+
+    this.set('headTags', headTags);
+  },
 });
