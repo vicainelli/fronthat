d654fd99c3ef5f9d5a5bfb244c8e17a9193e2470
Karl Ranna <karl@karlranna.com>
2017-06-14 08:59:13 +0300

    Add meta tags to post-a-job route

--- app/post-a-job/route.js
+++ app/post-a-job/route.js
@@ -1,9 +1,107 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel: function(model) {
+    this.setHeadTags(model);
+  },
+  setHeadTags: function() {
+    const description = 'Reach hundreds of frontend developers and designers. Post a job to FrontHAT.';
+    var headTags = [
+      {
+        type: 'meta',
+        tagId: 'meta-og-title',
+        attrs: {
+          name: 'og:title',
+          content: 'Post a Job to FrontHAT',
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-twitter-title',
+        attrs: {
+          name: 'twitter:title',
+          content: 'Post a Job to FrontHAT',
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'description',
+        attrs: {
+          name: 'description',
+          content: description
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-description',
+        attrs: {
+          name: 'og:description',
+          content: description
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-twitter-description',
+        attrs: {
+          name: 'twitter:description',
+          content: description
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-url',
+        attrs: {
+          name: 'og:url',
+          content: 'https://fronthat.com/post-a-job'
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-siteName',
+        attrs: {
+          name: 'og:site_name',
+          content: 'FrontHAT'
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-twitter-site',
+        attrs: {
+          name: 'twitter:site',
+          content: '@thefronthat'
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-twitter-card',
+        attrs: {
+          name: 'twitter:card',
+          content: 'summary',
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-og-image',
+        attrs: {
+          name: 'og:image',
+          content: 'https://fronthat.com/assets/images/icons/touch-icon-256x256.png',
+        }
+      },
+      {
+        type: 'meta',
+        tagId: 'meta-twitter-image',
+        attrs: {
+          name: 'twitter:image',
+          content: 'https://fronthat.com/assets/images/icons/touch-icon-256x256.png',
+        }
+      },
+    ];
+
+    this.set('headTags', headTags);
+  },
   actions: {
     postAJobSuccess() {
       this.transitionTo('post-a-job-success');
     }
-  }
+  },
 });
